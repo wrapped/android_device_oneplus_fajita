78a70c5e577fb761a4ea718a4b26de624e8a91d7
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-12-13 08:06:11 +0000

    fajita: BoardConfig: Commonize defconfig
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,8 +23,5 @@ DEVICE_PATH := device/oneplus/fajita
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Kernel
-TARGET_KERNEL_CONFIG := fajita_defconfig
-
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
